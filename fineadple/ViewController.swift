79053dcb8ccef00202b19464483059872453e9f0
yjyhorse <yjyhorse@yjyhorses-mac.local>
2019-03-27 11:18:45 +0900

    Initial Commit

---
+++ fineadple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fineadple
+//
+//  Created by yjyhorse on 2019. 3. 27..
+//  Copyright © 2019년 herren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
